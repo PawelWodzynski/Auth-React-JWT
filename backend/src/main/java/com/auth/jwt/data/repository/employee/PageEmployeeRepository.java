19c3adffba2ec95568eba0c47afda4e9b023062a
Pawel Wodzynski <p.wodzynski@optiq.net.pl>
2025-04-23 11:13:47 +0200

    setup auth

--- backend/src/main/java/com/auth/jwt/data/repository/employee/PageEmployeeRepository.java
+++ backend/src/main/java/com/auth/jwt/data/repository/employee/PageEmployeeRepository.java
@@ -1,14 +1,12 @@
-package com.workflow.WorkFlowDEMO.data.repository.employee;
+package com.auth.jwt.data.repository.employee;
 
-import com.workflow.WorkFlowDEMO.data.entity.employee.Employee;
-import io.swagger.v3.oas.annotations.Hidden;
+import com.auth.jwt.data.entity.employee.Employee;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@Hidden
 public interface PageEmployeeRepository extends JpaRepository<Employee,Integer> {
 
 
